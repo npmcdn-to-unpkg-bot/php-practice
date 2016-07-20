007f149726912d94c2b12f4c60bab4494a90d589
sutobu000 <sutou000@gmail.com>
2016-07-20 18:56:24 +0900

    edited timeline / add bootstrap

--- tweeting/timeline/login.php
+++ tweeting/timeline/login.php
@@ -6,6 +6,11 @@ require_once 'twitteroauth/autoload.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
+$force_login = "";
+if(!empty($_GET['force_login'])){
+  $force_login = "&force_login=true";
+}
+
 //TwitterOAuthのインスタンスを生成し、Twitterからリクエストトークンを取得する
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
 $request_token = $connection->oauth("oauth/request_token", array("oauth_callback" => OAUTH_CALLBACK));
@@ -16,4 +21,4 @@ $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 
 // Twitterの認証画面へリダイレクト
 $url = $connection->url("oauth/authorize", array("oauth_token" => $request_token['oauth_token']));
-header('Location: ' . $url);
+header('Location: ' . $url . "&force_login=true");
--- tweeting/timeline/more.php
+++ tweeting/timeline/more.php
@@ -10,9 +10,12 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 $user_connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['access_token']['oauth_token'], $_SESSION['access_token']['oauth_token_secret']);
 $user_info = $user_connection->get('account/verify_credentials');
 
+
+$message = $_SESSION['message'];
+$tweetup = $user_connection->post('statuses/update' , array( 'status' => $message));
+
 $params = [ 'count' => 21 ];
 
-	
 try{
 	if(isset($_GET['max_id'])) {
 		$params['max_id'] = $_GET['max_id'];
@@ -26,9 +29,22 @@ try{
 }
 
 array_pop($tweets);
-
+//var_dump($tweets);
 foreach ($tweets as $tweet) {
-	echo '<li id="tweet_' . $tweet->id_str . '">'. $tweet->user->name . ' : @' . $tweet->user->screen_name . '<br>' . $tweet->text . '</li>';
+	echo '
+<li id="tweet_'. $tweet->id_str .'" class="media" style="background-image:url(' . $tweet->user->profile_background_image_url_https . '); ">
+	<div class="mask" style="background-color:rgba(255, 255, 255, 0.7)">
+		<a class="media-left" href="https://twitter.com/' . $tweet->user->screen_name . '">
+			<img src="' . $tweet->user->profile_image_url_https . '">
+		</a>' . '
+		<div class="media-body">
+			<a href="https://twitter.com/' . $tweet->user->screen_name . '">
+				<h4 class="media-heading">' . $tweet->user->name . ' : @' . $tweet->user->screen_name . '</h4>
+			</a>
+			<br><p>' . $tweet->text . '</p>
+		</div>
+	</div>
+</li>';
 }
 
 echo "<br>";
